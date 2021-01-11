7bc2ac733c18b804d1b0ca93c13bb0ec19623bda
ravi-pathekar <ravi.pathekar@rapidops.com>
2021-01-11 15:28:04 +0530

    Updated Review Model

--- src/modules/review/review.model.js
+++ src/modules/review/review.model.js
@@ -2,30 +2,36 @@ const mongoose = require("mongoose");
 
 const ReviewSchema = new mongoose.Schema(
   {
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "user",
-      required: true,
-    },
     serviceProviderId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "serviceProvider",
       required: true,
     },
-    rating: {
-      type: Number,
-      max: 5,
-    },
-    comment: {
-      type: String,
-      maxlength: 255,
-      trim: true,
-      required: true,
-    },
+    reviews: [
+      {
+        userId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "serviceProvider",
+          required: true,
+        },
+        rating: {
+          type: Number,
+          max: 5,
+        },
+        comment: {
+          type: String,
+          maxlength: 255,
+          trim: true,
+          required: true,
+        },
+      },
+    ],
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+  }
 );
 
-const Review = mongoose.model("reviews", ReviewSchema);
+const Review = mongoose.model("review", ReviewSchema);
 
 module.exports = Review;
