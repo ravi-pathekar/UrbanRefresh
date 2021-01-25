6032668de610b512cdefff2bf3237204cad5af76
ravi-pathekar <ravi.pathekar@rapidops.com>
2021-01-25 17:32:16 +0530

    Deleted

--- src/modules/cities/cities.controller.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const City = require("./cities");
-
-router.post("/addCity", City.addCities);
-
-router.get("/getCity", City.getCities);
-
-module.exports = router;
--- src/modules/memberships/membership.controller.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const Membership = require("./membership");
-
-router.post("/addMembership", Membership.addMembership);
-
-router.get("/getMembership", Membership.getMembership);
-
-module.exports = router;
