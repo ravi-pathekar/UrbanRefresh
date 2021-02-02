ac50fbbbfbca12fa52ee6656c8bad2df2d22600d
ravi-pathekar <ravi.pathekar@rapidops.com>
2021-02-02 11:03:33 +0530

    Modified Faq model

--- src/modules/faq/faq.model.js
+++ src/modules/faq/faq.model.js
@@ -4,6 +4,17 @@ const FaqSchema = new mongoose.Schema(
   {
     question: { type: String, trim: true, required: true },
     answer: { type: String, trim: true, required: true },
+    questionType: {
+      type: String,
+      enum: ["serviceRelated", "websiteRelated"],
+      required: true,
+    },
+    serviceId: {
+      type: String,
+      required: function () {
+        if (this.questionType === "serviceRelated") return true;
+      },
+    },
   },
   { timestamps: true }
 );
